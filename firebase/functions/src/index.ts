5dcbe993fa6dbe6a5071cb83d9c945220d205285
takusamar <takusamar@gmail.com>
2021-04-05 09:31:38 +0900

    実行周期を10分毎から12分毎に変更。 Google Map APIの無料枠に収めるため。

--- firebase/functions/src/index.ts
+++ firebase/functions/src/index.ts
@@ -3,11 +3,26 @@ import * as functions from "firebase-functions";
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 //
+const dayjs = require("dayjs");
+const tz = require("dayjs/plugin/timezone");
+const utc = require("dayjs/plugin/utc");
+
+dayjs.extend(utc);
+dayjs.extend(tz);
+dayjs.tz.setDefault("Asia/Tokyo");
+
 // export const helloWorld = functions.https.onRequest((request, response) => {
 //   functions.logger.info("Hello logs!", {
 //     structuredData: true,
 //   });
-//   response.send("Hello from Firebase! tksTrafficJam");
+
+//   // const timestamp = dayjs('2021022214591234', { format: 'MMDDHHmmss', utc: false });
+//   const timestamp = dayjs("2021022214591234", "YYYYMMDDHHmmss")
+//     .tz("Asia/Tokyo", true)
+//     .toDate();
+//   console.log(timestamp);
+
+//   response.send(`[timestamp] ${timestamp}`);
 // });
 
 const admin = require("firebase-admin");
@@ -19,7 +34,7 @@ admin.initializeApp({
 const collectInformation = require("../src/trafficJam");
 export const doCollectInformation = functions
   .region("asia-northeast2")
-  .pubsub.schedule("every 10 minutes")
+  .pubsub.schedule("every 12 minutes")
   .onRun(async (context) => {
     await collectInformation();
   });
